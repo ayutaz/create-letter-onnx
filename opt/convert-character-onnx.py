c2c5ec464a185bc19d8f0d99af3dcc2d5ef41129
yousan <ka1357amnbpdr@gmail.com>
2023-01-16 01:55:30 +0900

    feat: 時間計測および精度向上

--- opt/convert-character-onnx.py
+++ opt/convert-character-onnx.py
@@ -1,9 +1,12 @@
-import torch
+import datetime
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 import torch.onnx
 import torch.nn.functional as F
+
+time = datetime.datetime.now()
+print("start time:", time.now())
 
 # load EMNIST dataset
 emnist_data = datasets.EMNIST(
@@ -22,13 +25,16 @@ print("EMNIST dataset loaded")
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(28 * 28, 64)
-        self.dropout = nn.Dropout(p=0.2)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 47)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 128, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(128 * 7 * 7, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 27)
 
     def forward(self, x):
-        x = x.view(-1, 28 * 28)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 128 * 7 * 7)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -51,24 +57,14 @@ for epoch in range(10):
 #
 # convert to ONNX format
 print("Converting to ONNX format")
-torch.onnx.export(model, torch.randn(1, 1, 28, 28), "model.onnx")
+torch.onnx.export(model, torch.randn(1, 1, 28, 28), "model-letters.onnx")
 
 # split train and validation data
 train_size = int(len(emnist_data) * 0.8)
 val_size = len(emnist_data) - train_size
 train_dataset, val_dataset = torch.utils.data.random_split(emnist_data, [train_size, val_size])
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=True)
-
-# train model
-for epoch in range(10):
-    for data, target in train_loader:
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
 
 # Evaluate on validation set
 val_loss = 0
@@ -84,3 +80,4 @@ val_loss /= len(val_dataset)
 val_acc = correct / len(val_dataset)
 
 print('Validation set: Average loss: {:.4f}, Accuracy: {:.2f}%'.format(val_loss, val_acc * 100))
+print("end time:", time.now(), ",duration:", time.now() - time)
